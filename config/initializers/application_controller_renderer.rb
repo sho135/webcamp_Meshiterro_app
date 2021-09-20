95762ac238d38f71a4b36adb2c1bb16d8ac25082
sho135 <sh135pro@gmail.com>
2021-09-20 22:41:20 +0000

    [Finish]4-3-6

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e76f814e67f9d0e9d49d4f3f122b1f825d5261430e43097551fbf61c26dd10cc41b9e02c6c69aee140ac14fc65b75bd1e1350cabce8c7a7b6b8e81768a5a8b70'
